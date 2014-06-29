8a14511b92af8c0b804e0f59773b988f783d6905
David de Bie <daviddebie09@gmail.com>
2014-06-29 16:24:09 +0200

    Add checker for correct guesses, checker for won game, congrats dialog

--- src/nl/mprog/apps/Hangman/GameplayActivity.java
+++ src/nl/mprog/apps/Hangman/GameplayActivity.java
@@ -1,6 +1,7 @@
 package nl.mprog.apps.Hangman;
 
 import java.util.Random;
+import android.view.View;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -9,8 +10,11 @@ import android.widget.TextView;
 import android.content.res.Resources;
 import android.widget.LinearLayout;
 import android.widget.GridView;
-
-
+import android.view.ViewGroup.LayoutParams;
+import android.view.Gravity;
+import android.graphics.Color;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 
 public class GameplayActivity extends ActionBarActivity {
 
@@ -20,6 +24,9 @@ public class GameplayActivity extends ActionBarActivity {
 	private LinearLayout wordLayout;
 	private GridView letters;
 	private LetterAdapter letterAdapter;
+	private TextView[] charViews;
+	private int numCorr;
+	private int numChars;
 	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,7 +47,7 @@ public class GameplayActivity extends ActionBarActivity {
         // get letter buttons
         letters = (GridView)findViewById(R.id.letters);
         
-        // start game
+        // start gameplay helper
         playGame();
     }
     
@@ -55,11 +62,91 @@ public class GameplayActivity extends ActionBarActivity {
     	// set new word to current word
     	newWord = currentWord;
     	
+    	// create separate textview for each character of the answer word
+    	charViews = new TextView[currentWord.length()];
+    	
+    	// remove views from previous game
+    	wordLayout.removeAllViews();
+    	
+    	// loop over each character of the answer word
+    	for (int i = 0; i < currentWord.length(); i++) {
+    		
+    		// create text view for each letter
+    	    charViews[i] = new TextView(this);
+    	    
+    	    // set text view to correct letter
+    		charViews[i].setText(""+currentWord.charAt(i));
+    		
+    		// set display properties
+    		charViews[i].setLayoutParams(new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT));
+    		charViews[i].setGravity(Gravity.CENTER);
+    		charViews[i].setTextColor(Color.WHITE);
+    		charViews[i].setBackgroundResource(R.drawable.letter_bg);
+    		wordLayout.addView(charViews[i]);
+    	}
+    	
     	letterAdapter = new LetterAdapter(this);
     	letters.setAdapter(letterAdapter);
     }
     	
 
+    public void letterPressed(View view) {
+    	// receive string from pressed letter button
+    	String ltr=((TextView)view).getText().toString();
+    	
+    	// get character from string
+		char letterChar = ltr.charAt(0);
+		
+		// disable used letter button and make it grey
+		view.setEnabled(false);
+		view.setBackgroundResource(R.drawable.letter_down);
+		
+		// set correctness checker to false
+		boolean correct = false;
+		
+		// loop over each character in answer word and compare to pressed letter
+		for(int j = 0; j < currentWord.length(); j++) {
+		  if(currentWord.charAt(j)==letterChar){
+		    correct = true;
+		    numCorr++;
+		    charViews[j].setTextColor(Color.BLACK);
+		  }
+		}
+		
+		// check if player has won the game after a correct guess
+		if(correct) {
+			if(numCorr == currentWord.length()) {
+				// Disable Buttons
+				  disableButtons();
+				 
+				  // Display Alert Dialog
+				  AlertDialog.Builder winBuild = new AlertDialog.Builder(this);
+				  winBuild.setTitle("Congratulations!");
+				  winBuild.setMessage("You won!\n\nThe answer was:\n\n"+currentWord);
+				  winBuild.setPositiveButton("Play Again",
+				    new DialogInterface.OnClickListener() {
+				      public void onClick(DialogInterface dialog, int id) {
+				        GameplayActivity.this.playGame();
+				    }});
+				 
+				  winBuild.setNegativeButton("Exit",
+				    new DialogInterface.OnClickListener() {
+				      public void onClick(DialogInterface dialog, int id) {
+				        GameplayActivity.this.finish();
+				    }});
+				 
+				  winBuild.show();
+			}
+		}
+    }
+    
+  //disable letter buttons
+  	public void disableButtons(){	
+  		int numLetters = letters.getChildCount();
+  		for(int l=0; l<numLetters; l++){
+  			letters.getChildAt(l).setEnabled(false);
+  		}
+  	}
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
